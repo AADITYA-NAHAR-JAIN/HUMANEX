2a5284a93e607aae6371bf46a3d900422a174734
Anushka Sarkar <anushkasarkar792@gmail.com>
2026-04-03 12:01:31 +0530

    chore: update frontend API endpoint to deployed backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 export const verifyUser = async (frames, challenge) => {
-  const res = await fetch("http://127.0.0.1:8000/verify", {
+  const res = await fetch("https//:humanex.onrender.com/verify", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ frames, challenge }),
